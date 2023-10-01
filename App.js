4f03fda0536f75aba407228c122d8a7b6c357854
huuquoc25 <huuquoc251203@gmail.com>
2023-10-01 11:29:50 +0700

    init project

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Houses from './screens/Houses';
+import BottomTap from './components/BottomTap';
+import AddReview from './screens/secondary/AddReview';
+import NewConstruc from './screens/secondary/NewConstruc';
+import Filters from './screens/secondary/Filters';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        // <View style={styles.container}>
+        //     <Filters />
+        // </View>
+        <BottomTap />
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 44,
+    },
 });
